ad7e5d13d816e6b839f22699c570548b9742c4a0
ziiiki <adriablancoculla@gmail.com>
2024-10-04 10:07:47 +0200

    ansatz.py

--- challenges/quantum_finance/solved/ansatz.py
+++ challenges/quantum_finance/solved/ansatz.py
@@ -3,10 +3,17 @@ from model_params import LAMBDA_1, LAMBDA_2, LAMBDA_3, NLAYERS, NSHOTS, NUM_ASSE
 from qibo import gates, models
 
 
-def compute_number_of_params(num_qubits: int, num_layers: int) -> int:
-    return num_qubits * (2 * num_layers + 2 + num_layers)
-
 def build_hardware_efficient_ansatz(num_qubits: int = N, num_layers: int = NLAYERS, two_gate: str = "CNOT") -> models.Circuit:
+    """Generates a HWEA with the same structure as in FIG 2. 
+
+    Args:
+        num_qubits (int, optional): _description_. Defaults to N.
+        num_layers (int, optional): _description_. Defaults to NLAYERS.
+        two_gate (str, optional): _description_. Defaults to "CNOT".
+
+    Returns:
+        models.Circuit: ansatz
+    """
     c = models.Circuit(num_qubits)
     c.add([gates.U2(qubit, 0, 0) for qubit in range(num_qubits)])
     for _ in range(num_layers):
@@ -16,3 +23,8 @@ def build_hardware_efficient_ansatz(num_qubits: int = N, num_layers: int = NLAYE
             
     c.add([gates.M(qubit) for qubit in range(num_qubits)])
     return c
+
+def compute_number_of_params_hwea(num_qubits: int, num_layers: int) -> int:
+    """Calculates the number of parameters (angles of rotation of the qubits) of the Hardware efficient ansatz (FIG 2) depending on the number of qubits and layers.
+    """
+    return num_qubits * (2 * num_layers + 2 + num_layers)
