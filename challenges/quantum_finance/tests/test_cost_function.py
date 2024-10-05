bf6b36faa32ff5111c68e15f5c89ec87eb8825a3
Alex Monras <amb.physis@gmail.com>
2024-10-05 16:25:28 +0200

    monkey patch

--- challenges/quantum_finance/tests/test_cost_function.py
+++ challenges/quantum_finance/tests/test_cost_function.py
@@ -6,24 +6,6 @@ import pandas as pd
 import numpy as np
 
 
-def generate_gaussian_dataframe(n_rows: int, n_columns: int):
-    # Create date range for index
-    date_index = pd.date_range(start='2023-01-01', periods=n_rows, freq='D')
-
-    # Generate gaussian distributed data
-    data = np.random.normal(0, 1, size=(n_rows, n_columns))
-
-    # Create dataframe with date index and I columns
-    df = pd.DataFrame(data, index=date_index, columns=[f'Col_{i + 1}' for i in range(n_columns)])
-
-    return df
-
-
-# Example usage: create a DataFrame with 10 rows and 3 columns
-df_example = generate_gaussian_dataframe(10, 3)
-print(df_example)
-
-
 class TestCostFunction:
     def test_binary_encoding_lsd(self):
         """
