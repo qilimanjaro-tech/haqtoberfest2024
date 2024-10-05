16d67f2b5c29a7b0222fd23d8ada6c13b8454a8b
ForlokACG <alcane311@gmail.com>
2024-10-05 14:28:17 +0200

    Good layer for the anzatz

--- challenges/quantum_finance/challenge/ansatz.py
+++ challenges/quantum_finance/challenge/ansatz.py
@@ -14,22 +14,16 @@ def build_hardware_efficient_ansatz(num_qubits: int = N, num_layers: int = NLAYE
     Returns:
         models.Circuit: ansatz
     """
+    def add_layer(): #layer definition
+        c.add((gates.U2(q, 0, 0) for q in range(num_qubits))) #Column of U2
+        if two_gate="CNOT": c.add((gates.CNOT(q, q+1) for q in range(num_qubits-1))) #CNOT Mesh
+        if two_gate="CZ": c.add((gates.CZ(q, q+1) for q in range(num_qubits-1))) #CZ Mesh
+        c.add((gates.U1(q, 0) for q in range(num_qubits))) #Column of U1
+
     c = models.Circuit(num_qubits)
-
     c.add((gates.U2(q, 0, 0) for q in range(num_qubits))) #First column of U2
-    c.add((gates.U2(q, 0, 0) for q in range(num_qubits))) #Second column of U2
-    c.add((gates.CNOT(q, q+1) for q in range(num_qubits-1))) #First CNOT Mesh
-
+    for _ in range(num_layers): add_layer()
     c.add((gates.U1(q, 0) for q in range(num_qubits))) #First column of U1
-    c.add((gates.U2(q, 0, 0) for q in range(num_qubits))) #Third column of U2
-    c.add((gates.CNOT(q, q+1) for q in range(num_qubits-1))) #Second CNOT Mesh
-
-    c.add((gates.U1(q, 0) for q in range(num_qubits))) #Second column of U1
-    c.add((gates.U2(q, 0, 0) for q in range(num_qubits))) #Fourth column of U2
-    c.add((gates.CNOT(q, q+1) for q in range(num_qubits-1))) #Third CNOT Mesh
-
-    c.add((gates.U1(q, 0) for q in range(num_qubits))) #Third column of U1
-
     c.add((gates.M(q) for q in range(num_qubits))) #Column of qubit Measurement
 
     return c
@@ -37,4 +31,5 @@ def build_hardware_efficient_ansatz(num_qubits: int = N, num_layers: int = NLAYE
 def compute_number_of_params_hwea(num_qubits: int, num_layers: int) -> int:
     """Calculates the number of parameters (angles of rotation of the qubits) of the Hardware efficient ansatz (FIG 2) depending on the number of qubits and layers.
     """
+
     return 
